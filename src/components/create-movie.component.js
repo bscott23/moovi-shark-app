23d2b2932ec70fc7c150f3de243d8dc2da709f47
board-man <brodyscott12@gmail.com>
2019-08-05 06:13:04 -0400

    Initialized front-end components

---
+++ src/components/create-movie.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateMovie extends Component {
+  render() {
+    return (
+      <div>
+        <p>Create: Add Movie</p>
+      </div>
+    )
+  }
+}
---
+++ src/components/movies-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MoviesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Read: List All Movies</p>
+      </div>
+    )
+  }
+}
