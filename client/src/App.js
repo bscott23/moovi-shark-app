50caa5bcfb81437c50f8c63d6e01df592e9143cb
board-man <brodyscott12@gmail.com>
2019-08-09 01:01:14 -0400

    DiscoverMovies component now renders movies with bootstrap card decks

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Navbar from "./components/navbar.component"
-import MoviesList from "./components/movies-list.component";
-import CreateMovie from "./components/create-movie.component";
-import CreateUser from "./components/create-user.component";
+import Navbar from "./components/Navbar"
+import MoviesList from "./components/Watchlist";
+import DiscoverMovies from "./components/DiscoverMovies";
+import CreateUser from "./components/CreateUser";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Navbar />
         <br/>
         <Route path="/" exact component={MoviesList} />
-        <Route path="/create" component={CreateMovie} />
+        <Route path="/discover" component={DiscoverMovies} />
         <Route path="/user" component={CreateUser} />
       </div>
     </Router>
---
+++ client/src/components/DiscoverMovies.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import Button from "react-bootstrap/Button";
+
+class DiscoverMovies extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      discover: {}
+    };
+  }
+
+  async componentDidMount() {
+    let res = await fetch("http://localhost:5000/discover/");
+    this.setState({ discover: await res.json(), isLoading: false });
+  }
+
+  mapMovieCards = () => {
+
+    const z = this.state.discover.length - this.state.discover.length % 3;
+    const cardsPerDeck = 3;
+    const cardDecks = [];
+
+    // ultimately want an array of card decks
+
+    for (let i = 0; i < z; i += cardsPerDeck) {
+      let movieSubset = this.state.discover.slice(i, i + cardsPerDeck);
+      let cardSubset = movieSubset.map(movie => (
+        <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
+              <Card.Body>
+              <Card.Title>{movie.title}</Card.Title>
+              <Card.Text>{movie.overview}</Card.Text>
+              <Button variant="outline-primary">Add</Button>
+              </Card.Body>
+              </Card>
+      ))
+
+      const cardDeck = (<CardDeck>{cardSubset}</CardDeck>);
+
+      cardDecks.push(cardDeck);
+    }
+
+    return (
+      <div>
+        {cardDecks}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Content: Discover page for new movies</p>
+        <div>{this.state.isLoading ? (
+            <h1>Loading...</h1>
+          ) : this.mapMovieCards()}</div>
+        <br style = {{"line-height":50}}></br>
+        <div textAlign="center"><Button variant="primary" block>Watchlist</Button></div>
+        <br style = {{"line-height":50}}></br>
+      </div>
+    );
+  }
+}
+
+export default DiscoverMovies;
